397b154cf98a8159b9d6f22f87e43a236975f118
josterga <josterga@uci.edu>
2024-05-31 22:30:35 -0700

    ux changes, fix substitution score bug

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,14 +22,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "f5ea1453d3ba6edf045d3ff28d58d529",
+"flutter_bootstrap.js": "c0c27484a04f9888ff5227dd2933220d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "36f8995b3db6de68ce2d541135f9a5d0",
 "/": "36f8995b3db6de68ce2d541135f9a5d0",
-"main.dart.js": "3754b3a5df35918156aa58b09d1ff05b",
+"main.dart.js": "7c234175fe5e15ab698cae6f7ad0e53f",
 "manifest.json": "d117001ba10860cafc6ea6298be9a187",
 "version.json": "ee0486d1d5b095c6c1d9c6c2c63aab15"};
 // The application shell files that are downloaded before a service worker can
